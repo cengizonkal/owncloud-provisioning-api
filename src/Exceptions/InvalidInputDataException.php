741d0181203d50105d96e1d4519bccbc09434746
cengiz <onkal.cengiz@gmail.com>
2023-04-02 00:39:11 +0300

    added missing exception messages

--- src/Exceptions/InvalidInputDataException.php
+++ src/Exceptions/InvalidInputDataException.php
@@ -5,4 +5,9 @@ namespace Conkal\OwncloudProvisioningApi\Exceptions;
 class InvalidInputDataException extends \Exception
 {
 
+    public function __construct($message = "Invalid Input Data", $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
 }
